4400e5486694ffd11f1c3e1b0e0242a7e1d78f87
Albina Shipova <volare.al@gmail.com>
2020-11-11 12:40:01 +0300

    04_fractal. рисуем рекурсивное дерево с рандомными ветвями.

--- skillbox_shandrinov/lesson_004/04_fractal.py
+++ skillbox_shandrinov/lesson_004/04_fractal.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
 # - точка начала рисования,
@@ -26,7 +27,20 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
+def draw_branches(start_point, angle=90, length=100):
+    if length < 10:
+        return
+    v1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=2)
+    v1.draw()
+    start_point = v1.end_point
+    length = length * 0.75
+    draw_branches(start_point, angle=angle + 30, length=length)
+    draw_branches(start_point, angle=angle - 30, length=length)
+
+
+root_point = sd.get_point(300, 30)
+draw_branches(start_point=root_point, angle=90, length=100)
+
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
@@ -36,6 +50,25 @@ import simple_draw as sd
 # Пригодятся функции
 # sd.random_number()
 
+def draw_branches_random(start_point, angle=90, length=100):
+    if length < 10:
+        return
+    v1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=2)
+    v1.draw()
+    start_point = v1.end_point
+
+    deviation = 30
+    angle_deviation = deviation * 0.4
+    angle_random = deviation + sd.random_number(0, angle_deviation)
+
+    length_deviation = round(0.75 * 0.2)
+    length = length * (0.75 + sd.random_number(0, length_deviation))
+
+    draw_branches_random(start_point, angle=angle + angle_random, length=length)
+    draw_branches_random(start_point, angle=angle - angle_random, length=length)
+
+
+root_point = sd.get_point(300, 0)
+draw_branches_random(start_point=root_point, angle=90, length=100)
+
 sd.pause()
-
-
