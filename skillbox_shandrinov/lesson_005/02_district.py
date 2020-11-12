8a7d9c9a741fb6f265905bbd499c9dc1dc301902
Albina Shipova <volare.al@gmail.com>
2020-11-12 22:08:10 +0300

    02. импорт многоуровневых одноименных пакетов

--- skillbox_shandrinov/lesson_005/02_district.py
+++ skillbox_shandrinov/lesson_005/02_district.py
@@ -5,7 +5,14 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1 import room1 as csh1r1, room2 as csh1r2
+from district.central_street.house2 import room1 as csh2r1, room2 as csh2r2
+from district.soviet_street.house1 import room1 as ssh1r1, room2 as ssh1r2
+from district.soviet_street.house2 import room1 as ssh2r1, room2 as ssh2r2
+citizen = []
+for i in [csh1r1, csh1r2, csh2r1, csh2r2, ssh1r1, ssh1r2, ssh2r1, ssh2r2]:
+    citizen.extend(i.folks)
+print('На районе живут', ', '.join(citizen))
 
 
 
